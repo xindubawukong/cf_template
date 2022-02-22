1037eaf8ea35f96e8d7e5423ca6ecee9bf35f85b
xindubawukong <xindubawukong@gmail.com>
2022-02-22 22:34:17 +0800

    first

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  // This is a template.
+  return 0;
+}
