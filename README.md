548578007e5e626c4e1669b59e9e63cc442e50e0
xindubawukong <xindubawukong@gmail.com>
2022-03-26 00:11:55 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Template for writing algorithms in C++.
+
+Please select 64bit compiler.
--- aaa.cpp
+++ aaa.cpp
@@ -1,4 +1,6 @@
+#ifdef __clang__
 #define LOCAL
+#endif
 
 #include <algorithm>
 #include <cassert>
@@ -19,11 +21,19 @@
 
 using namespace std;
 
+using float64 = long double;
+using int64 = long long;
+using int128 = __int128_t;
+using uint64 = unsigned long long;
+
+// --------------------------- xindubawukong ---------------------------
+
 void Main() {}
 
 int main() {
 #ifdef LOCAL
   freopen("../input.txt", "r", stdin);
+  // freopen("../output.txt", "w", stdout);
 #endif
   Main();
   return 0;
