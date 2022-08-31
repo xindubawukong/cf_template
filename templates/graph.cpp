2eda06cf0ca7996efc21f9907b594e128b4bfa43
xindubawukong <xindubawukong@gmail.com>
2022-09-01 04:37:19 +0800

    add cut vertices

--- templates/graph.cpp
+++ templates/graph.cpp
@@ -1,5 +1,4 @@
-// cut edges
-vector<bool> GetCut(int n, int m, auto& go) {
+vector<bool> GetCutEdges(int n, int m, auto& go) {
   vector<int> low(n), dfn(n);
   vector<bool> vt(n);
   int ts = 0;
@@ -18,6 +17,37 @@ vector<bool> GetCut(int n, int m, auto& go) {
       } else {
         low[x] = min(low[x], dfn[y]);
       }
+    }
+  };
+  for (int i = 0; i < n; i++) {
+    if (!vt[i]) {
+      Dfs(Dfs, i, -1);
+    }
+  }
+  return cut;
+}
+
+vector<bool> GetCutVertices(int n, int m, auto& go) {
+  vector<int> low(n), dfn(n);
+  vector<bool> vt(n);
+  int ts = 0;
+  vector<bool> cut(n);
+  auto Dfs = [&](auto self, int x, int from) -> void {
+    dfn[x] = low[x] = ++ts;
+    vt[x] = true;
+    int num = 0;
+    for (int y : go[x]) {
+      if (y == from) continue;
+      if (!vt[y]) {
+        self(self, y, x);
+        low[x] = min(low[x], low[y]);
+        if (low[y] >= dfn[x]) num++;
+      } else {
+        low[x] = min(low[x], dfn[y]);
+      }
+    }
+    if ((x == 0 && num > 1) || (x > 0 && num > 0)) {
+      cut[x] = true;
     }
   };
   for (int i = 0; i < n; i++) {
