3027b130e8df4b3068ece0e082c71229d13b6f33
xindubawukong <xindubawukong@gmail.com>
2023-04-07 12:36:29 +0800

    add

--- templates/dsu.h
+++ templates/dsu.h
@@ -4,8 +4,6 @@
 #include <stack>
 #include <vector>
 
-#include "debug.h"
-
 template <bool Compress = true, bool SupportUndo = false>
 struct Dsu {
   static_assert(!Compress || (Compress && !SupportUndo));
@@ -46,6 +44,78 @@ struct Dsu {
     }
     fa[x] = y;
     size[y] += size[x];
+    cnt--;
+  }
+  void Undo(int cnt) {
+    static_assert(SupportUndo);
+    assert(0 <= cnt && cnt <= undo.size());
+    for (int i = 0; i < cnt; i++) {
+      undo.top()();
+      undo.pop();
+    }
+  }
+};
+
+template <bool Compress = true, bool SupportUndo = false,
+          typename Relation = int, typename Plus = std::plus<Relation>,
+          typename Negate = std::negate<Relation>>
+struct DsuWithRelation {
+  static_assert(!Compress || (Compress && !SupportUndo));
+  std::vector<int> fa, size;
+  int n, cnt;
+  std::stack<std::function<void()>> undo;
+  std::vector<Relation> rel;
+  Plus plus;
+  Negate negate;
+  DsuWithRelation(int n_, Plus plus_, Negate negate_)
+      : n(n_), plus(plus_), negate(negate_) {
+    fa.resize(n);
+    size.resize(n);
+    rel.resize(n);
+    for (int i = 0; i < n; i++) {
+      fa[i] = i;
+      size[i] = 1;
+    }
+    cnt = n;
+  }
+  std::pair<int, Relation> Find(int x) {
+    assert(0 <= x && x < n);
+    if (fa[x] == x) return {x, rel[x]};
+    auto [root, relfa] = Find(fa[x]);
+    if (Compress) {
+      fa[x] = root;
+      rel[x] = plus(rel[x], relfa);
+      return {root, rel[x]};
+    } else {
+      return {root, plus(rel[x], relfa)};
+    }
+  }
+  void Unite(int x, int y, Relation relxy) {
+    assert(0 <= x && x < n && 0 <= y && y < n);
+    Relation tx, ty;
+    std::tie(x, tx) = Find(x);
+    std::tie(y, ty) = Find(y);
+    if (x == y) {
+      assert(relxy == plus(tx, negate(ty)));
+      return;
+    }
+    if (size[x] > size[y]) {
+      std::swap(x, y);
+      std::swap(tx, ty);
+      relxy = negate(relxy);
+    }
+    if (SupportUndo) {
+      int fax = fa[x], sizey = size[y];
+      undo.push([this, x, y, fax, sizey, tx]() {
+        fa[x] = fax;
+        size[y] = sizey;
+        rel[x] = tx;
+        cnt++;
+      });
+    }
+    fa[x] = y;
+    size[y] += size[x];
+    rel[x] = plus(relxy, plus(negate(tx), ty));
     cnt--;
   }
   void Undo(int cnt) {
--- tests/dsu_test.cpp
+++ tests/dsu_test.cpp
@@ -3,7 +3,6 @@
 #include <random>
 #include <vector>
 
-#include "debug.h"
 #include "gtest/gtest.h"
 
 TEST(DsuTest, BasicTest) {
@@ -40,4 +39,38 @@ TEST(DsuTest, UndoTest) {
     EXPECT_EQ(dsu.Find(i), i);
     EXPECT_EQ(1, dsu.size[i]);
   }
+}
+
+TEST(DsuTest, RelationTest) {
+  auto plus = [](int x, int y) { return x ^ y; };
+  auto negate = [](int x) { return x; };
+  int n = 10;
+  DsuWithRelation<true, false, int, decltype(plus), decltype(negate)> dsu(
+      n * 2, plus, negate);
+  std::mt19937 rng(
+      std::chrono::high_resolution_clock::now().time_since_epoch().count());
+  for (int i = 0; i < 30; i++) {
+    int x = rng() % n, y = rng() % n + n;
+    int rx, ry, tx, ty;
+    std::tie(rx, tx) = dsu.Find(x);
+    std::tie(ry, ty) = dsu.Find(y);
+    if (rx != ry) {
+      dsu.Unite(x, y, 1);
+    } else {
+      EXPECT_NE(tx, ty);
+    }
+  }
+}
+
+TEST(DsuTest, RelationUndoTest) {
+  auto plus = [](int x, int y) { return x ^ y; };
+  auto negate = [](int x) { return x; };
+  DsuWithRelation<false, true, int, decltype(plus), decltype(negate)> dsu(
+      4, plus, negate);
+  dsu.Unite(0, 2, 1);
+  dsu.Unite(0, 3, 1);
+  EXPECT_EQ(dsu.Find(2), dsu.Find(3));
+  dsu.Undo(1);
+  dsu.Unite(2, 3, 1);
+  EXPECT_NE(dsu.Find(2), dsu.Find(3));
 }
