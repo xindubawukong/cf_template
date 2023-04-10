3f01c9d34b8f58962adbea0557de3a5c630e4232
xindubawukong <xindubawukong@gmail.com>
2023-04-10 16:15:01 +0800

    upd

--- templates/get_primes.h
+++ templates/get_primes.h
@@ -3,22 +3,26 @@
 
 #include <vector>
 
-template <typename T>
-std::pair<std::vector<T>, std::vector<bool>> GetPrimes(T n) {
+template <typename T = int>
+std::tuple<std::vector<T>, std::vector<bool>, std::vector<T>> GetPrimes(T n) {
   std::vector<T> primes;
   std::vector<bool> is_prime(n + 1, true);
+  is_prime[0] = is_prime[1] = false;
+  std::vector<T> minp(n + 1);
   for (T i = 2; i <= n; i++) {
     if (is_prime[i]) {
       primes.push_back(i);
+      minp[i] = i;
     }
     for (T p : primes) {
       T x = p * i;
       if (x > n) break;
       is_prime[x] = false;
+      minp[x] = p;
       if (i % p == 0) break;
     }
   }
-  return std::make_pair(primes, is_prime);
+  return {primes, is_prime, minp};
 }
 
 #endif  // GET_PRIMES_H_
--- tests/get_primes_test.cpp
+++ tests/get_primes_test.cpp
@@ -5,6 +5,15 @@
 #include "gtest/gtest.h"
 
 TEST(GetPrimesTest, BasicTest) {
-  EXPECT_EQ(std::vector<int>({2, 3, 5, 7, 11, 13, 17, 19}),
-            GetPrimes(20).first);
+  auto [primes, is_prime, minp] = GetPrimes(20);
+  EXPECT_EQ(primes, std::vector<int>({2, 3, 5, 7, 11, 13, 17, 19}));
+  for (int i = 0; i <= 20; i++) {
+    if (std::find(primes.begin(), primes.end(), i) == primes.end()) {
+      EXPECT_FALSE(is_prime[i]);
+    } else {
+      EXPECT_TRUE(is_prime[i]);
+    }
+  }
+  EXPECT_EQ(minp, std::vector<int>({0,  0, 2,  3, 2, 5, 2,  7, 2,  3, 2,
+                                    11, 2, 13, 2, 3, 2, 17, 2, 19, 2}));
 }
