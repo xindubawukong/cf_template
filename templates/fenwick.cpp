a731367e350e5d5915de25cc1af72f725afd1160
xindubawukong <xindubawukong@gmail.com>
2023-07-22 16:03:24 +0800

    upd

--- templates/fenwick.cpp
+++ templates/fenwick.cpp
@@ -1,19 +1,18 @@
-template <typename T>
 struct Fenwick {
   int n;
-  vector<T> a;
+  vector<int> a;
 
   Fenwick(int n_) : n(n_) { a.resize(n); }
 
-  void Change(int x, T dx) {
+  void Change(int x, int dx) {
     while (x < n) {
       a[x] += dx;
       x |= (x + 1);
     }
   }
 
-  T GetSum(int x) {
-    T res = 0;
+  int GetSum(int x) {
+    int res = 0;
     while (x >= 0) {
       res += a[x];
       x = (x & (x + 1)) - 1;
@@ -21,7 +20,7 @@ struct Fenwick {
     return res;
   }
 
-  T RangeSum(int l, int r) {
+  int RangeSum(int l, int r) {
     auto res = GetSum(r);
     if (l > 0) res -= GetSum(l - 1);
     return res;
