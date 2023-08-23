064c86284baa882e3974d034af47d10ee0f1608a
xindubawukong <xindubawukong@gmail.com>
2023-08-23 22:02:12 +0800

    upd

--- templates/main.cpp
+++ templates/main.cpp
@@ -5,11 +5,6 @@ using namespace std;
 #else
 #define debug(...)
 #endif
-// clang-format off
-using int64 = long long;
-template <typename T> bool ckmin(T& a, T b) { return b < a ? a = b, 1 : 0; }
-template <typename T> bool ckmax(T& a, T b) { return b > a ? a = b, 1 : 0; }
-// clang-format on
 
 // ----------------------------------------------------------------------
 
