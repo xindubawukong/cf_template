8900be23656fc27aa2dade0530c2d3ae04da740c
xindubawukong <xindubawukong@gmail.com>
2023-08-24 13:18:56 +0800

    upd

--- templates/main.cpp
+++ templates/main.cpp
@@ -4,6 +4,11 @@
 #else
 #define debug(...)
 #endif
+using int64 = long long;
+using uint = unsigned int;
+using uint64 = unsigned long long;
+template <typename T> bool ckmin(T &a, T b) { return b < a ? a = b, 1 : 0; }
+template <typename T> bool ckmax(T &a, T b) { return b > a ? a = b, 1 : 0; }
 using namespace std;
 
 int main() {
