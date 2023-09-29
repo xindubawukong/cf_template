d0086fb93831b4875a64ca27e2d3c8530c857958
xindubawukong <xindubawukong@gmail.com>
2023-09-29 16:42:40 -0700

    upd

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ bool ckmin(auto& a, auto b) { return b < a ? a = b, 1 : 0; }
 bool ckmax(auto& a, auto b) { return b > a ? a = b, 1 : 0; }
 using namespace std;
 
-int32_t main() {
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
