17f4545efa6560588568512ae61b2290619553c5
xindubawukong <xindubawukong@gmail.com>
2024-08-06 22:53:38 -0400

    fk

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ bool ckmin(auto& a, auto b) { return b < a ? a = b, 1 : 0; }
 bool ckmax(auto& a, auto b) { return b > a ? a = b, 1 : 0; }
 using namespace std;
 
-int main(int argc, char** argv) {
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
