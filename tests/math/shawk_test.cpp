bc85b9b5d53d6d30d42e50c8371bb169c0d9fc91
xindubawukong <xindubawukong@gmail.com>
2024-11-13 17:13:44 -0800

    upd

--- tests/math/shawk_test.cpp
+++ tests/math/shawk_test.cpp
@@ -1,11 +1,10 @@
-#include "math/smawk.h"
-
 #include "gtest/gtest.h"
+#include "math/smawk.h"
 
 TEST(SMAWKTest, BasicTest) {
   int n = 99;
   auto f = [](int i, int j) { return (i - j) * (i - j); };
-  auto res = SMAWK(n, n, f);
+  auto res = SMAWK_RowMin(n, n, f);
   EXPECT_EQ(n, (int)res.size());
   for (int i = 0; i < n; i++) EXPECT_EQ(i, res[i]);
 }
